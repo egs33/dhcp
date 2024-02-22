3cfabdb3bd844787810ee73038a4e693398e48fa
egs33 <egs33.bicycle@gmail.com>
2024-02-22 23:55:04 +0900

    Add component to system

--- src/dhcp/core.clj
+++ src/dhcp/core.clj
@@ -1,5 +1,16 @@
 (ns dhcp.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [clojure.core.async :as as]
+   [dhcp.system :as system]))
 
 (defn -main
-  [& _args])
+  [& _args]
+  (let [{:keys [:udp-server]} (system/start)]
+    ;; block until the server is stopped
+    (as/<!! (as/go-loop []
+              (let [{:keys [:socket]} udp-server]
+                (when (and socket
+                           (not (.isClosed socket)))
+                  (as/<! (as/timeout 1000))
+                  (recur)))))))
--- src/dhcp/system.clj
+++ src/dhcp/system.clj
@@ -3,15 +3,17 @@
    [aero.core :as aero]
    [clojure.java.io :as io]
    [com.stuartsierra.component :as component]
+   [dhcp.components.udp-server :as c.udp-server]
    [unilog.config :as unilog]))
 
 (defn- new-system [_config]
-  (component/system-map))
+  (component/system-map
+   :udp-server (c.udp-server/->UdpServer nil)))
 
 (defn start []
   (let [config (aero/read-config (io/resource "config.edn") {:profile :prod})
+        _ (unilog/start-logging! (:logging config))
         system (component/start (new-system config))]
-    (unilog/start-logging! (:logging config))
     system))
 
 (defn stop [system]
