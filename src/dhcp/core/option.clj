80b5faab2ffb4b3eecf1ed65c39e09dd748f3772
egs33 <egs33.bicycle@gmail.com>
2024-02-22 23:18:15 +0900

    Add option parser

---
+++ src/dhcp/core/option.clj
@@ -0,0 +1,113 @@
+(ns dhcp.core.option
+  (:require
+   [clojure.tools.logging :as log])
+  (:import
+   (clojure.lang
+    IPersistentVector)))
+
+(def ^:private option-code-map
+  "RFC2132"
+  {0 :pad
+   255 :end
+   1 :subnet-mask
+   2 :time-offset
+   3 :router-option
+   4 :time-server-option
+   5 :name-server-option
+   6 :domain-name-server-option
+   7 :log-server-option
+   8 :cookie-server-option
+   9 :lpr-server-option
+   10 :impress-server-option
+   11 :resource-location-server-option
+   12 :host-name-option
+   13 :boot-file-size-option
+   14 :merit-dump-file
+   15 :domain-name
+   16 :swap-server
+   17 :root-path
+   18 :extensions-path
+   19 :ip-forwarding-enable-disable-option
+   20 :non-local-source-routing-enable-disable-option
+   21 :policy-filter-option
+   22 :max-datagram-reassembly-size
+   23 :default-ip-time-to-live
+   24 :path-mtu-aging-timeout-option
+   25 :path-mtu-plateau-table-option
+   26 :interface-mtu-option
+   27 :all-subnets-local-option
+   28 :broadcast-address-option
+   29 :perform-mask-discovery-option
+   30 :mask-supplier-option
+   31 :perform-router-discovery-option
+   32 :router-solicitation-address-option
+   33 :static-route-option
+   34 :trailer-encapsulation-option
+   35 :arp-cache-timeout-option
+   36 :ethernet-encapsulation-option
+   37 :tcp-default-ttl-option
+   38 :tcp-keepalive-interval-option
+   39 :tcp-keepalive-garbage-option
+   40 :network-information-service-domain-option
+   41 :network-information-servers-option
+   42 :network-time-protocol-servers-option
+   43 :vendor-specific-information
+   44 :netbios-over-tcp-ip-name-server-option
+   45 :netbios-over-tcp-ip-datagram-distribution-server-option
+   46 :netbios-over-tcp-ip-node-type-option
+   47 :netbios-over-tcp-ip-scope-option
+   48 :x-window-system-font-server-option
+   49 :x-window-system-display-manager-option
+   50 :requested-ip-address
+   51 :ip-address-lease-time
+   52 :option-overload
+   53 :dhcp-message-type
+   54 :server-identifier
+   55 :parameter-request-list
+   56 :message
+   57 :maximum-dhcp-message-size
+   58 :renewal-time-value
+   59 :rebinding-time-value
+   60 :vendor-class-identifier
+   61 :client-identifier
+   64 :network-information-service-plus-domain-option
+   65 :network-information-service-plus-servers-option
+   66 :tftp-server-name-option
+   67 :bootfile-name-option
+   68 :mobile-ip-home-agent-option
+   69 :simple-mail-transport-protocol-server-option
+   70 :post-office-protocol-server-option
+   71 :network-news-transport-protocol-server-option
+   72 :default-world-wide-web-server-option
+   73 :default-finger-server-option
+   74 :default-internet-relay-chat-server-option
+   75 :streettalk-server-option
+   76 :streettalk-directory-assistance-server-option})
+
+(defn- parse-option
+  [^IPersistentVector bytes]
+  (let [code (Byte/toUnsignedInt (first bytes))]
+    (case (Byte/toUnsignedInt (first bytes))
+      0 {:code 0, :type :pad, :length 1, :value []}
+      255 {:code 255, :type :end, :length 1, :value []}
+      (let [len (Byte/toUnsignedInt (nth bytes 1))]
+        {:code code
+         :type (get option-code-map code :unknown)
+         :length (+ 2 len)
+         :value (subvec bytes 2 (+ 2 len))}))))
+
+(defn parse-options [^bytes bytes]
+  (let [[b1 b2 b3 b4 & rest] (seq bytes)]
+    (if (and (= b1 99)
+             (= b2 -126)                                  ; = 130
+             (= b3 83)
+             (= b4 99))
+      (loop [rest (vec rest)
+             options (transient [])]
+        (if (seq rest)
+          (let [option (parse-option rest)]
+            (recur (subvec rest (:length option))
+                   (conj! options option)))
+          (persistent! options)))
+      (log/warn "magic cookie not found" {:4octets [b1 b2 b3 b4]
+                                          :len (count bytes)}))))
---
+++ test/dhcp/core/option_test.clj
@@ -0,0 +1,35 @@
+(ns dhcp.core.option-test
+  (:require
+   [clojure.test :refer [deftest testing is]]
+   [dhcp.core.option :as option]))
+
+(def magic-cookie [99 130 83 99])
+
+(deftest parse-options-test
+  (testing "pad-option"
+    (is (= [{:code 0, :type :pad, :length 1, :value []}]
+           (option/parse-options
+            (byte-array (concat magic-cookie [0]))))))
+  (testing "end-option"
+    (is (= [{:code 255, :type :end, :length 1, :value []}]
+           (option/parse-options
+            (byte-array (concat magic-cookie [255]))))))
+  (testing "requested-ip-address"
+    (is (= [{:code 50, :type :requested-ip-address, :length 6, :value [-64 -88 0 1]}]
+           (option/parse-options
+            (byte-array (concat magic-cookie [50 4 192 168 0 1]))))))
+  (testing "multi options"
+    (is (= [{:code 53, :type :dhcp-message-type, :length 3, :value [3]}
+            {:code 61, :type :client-identifier, :length 8, :value [10 20 30 40 50 60]}
+            {:code 50, :type :requested-ip-address, :length 6, :value [-64 -88 0 1]}
+            {:code 255, :type :end, :length 1, :value []}]
+           (option/parse-options
+            (byte-array (concat magic-cookie [53 1 3
+                                              61 6 10 20 30 40 50 60
+                                              50 4 192 168 0 1
+                                              255]))))))
+  (testing "invalid magic cookie"
+    (is (nil? (option/parse-options
+               (byte-array [99 130 83 100
+                            255
+                            0]))))))
