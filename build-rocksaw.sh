380ba67f3a5af24cb47f172c61803882e1a28e5b
egs33 <egs33.bicycle@gmail.com>
2024-03-26 00:06:15 +0900

    Patch for raw socket includes ethernet header

--- build-rocksaw.sh
+++ build-rocksaw.sh
@@ -7,8 +7,7 @@ tar -xf rocksaw-1.1.0-src.tar.gz
 
 cd rocksaw-1.1.0
 
-sed -i "s/javac.source=1.3/javac.source=17/g" build.properties
-sed -i "s/javac.target=1.3/javac.target=17/g" build.properties
+patch -p1 < ../rocksaw-diff.patch
 
 JDK_HOME=$JAVA_HOME ant jar
 
