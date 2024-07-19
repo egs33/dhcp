675b4919f481f11ff96d553cabc6e8ee2b9bb35a
egs33 <egs33.bicycle@gmail.com>
2024-07-20 01:34:39 +0900

    Fix record class

--- src/dhcp/system.clj
+++ src/dhcp/system.clj
@@ -10,16 +10,16 @@
    [dhcp.records.config :as r.config]
    [unilog.config :as unilog]))
 
-(defn- new-system [_config server-config]
+(defn- new-system [_config {:as server-config :keys [:config]}]
   (component/system-map
-   :db (case (get-in server-config [:database :type])
+   :db (case (get-in config [:database :type])
          "memory" (db.mem/new-memory-database)
-         "postgresql" (db.pg/new-postgres-database (get-in server-config [:database :postgresql-option]))
+         "postgresql" (db.pg/new-postgres-database (get-in config [:database :postgresql-option]))
          (throw (IllegalArgumentException. (str "Unsupported database type: " type))))
    :handler (component/using
              (c.handler/map->Handler {:config server-config})
              [:db])
-   :udp-server (component/using (c.udp-server/map->UdpServer {:config server-config})
+   :udp-server (component/using (c.udp-server/map->UdpServer {:config config})
                                 [:handler])))
 
 (defn start [options]
@@ -28,7 +28,7 @@
                                    (:debug options) (assoc :level :debug)))
         server-config (r.config/load-config (:config options))]
     (when server-config
-      (component/start (new-system config (:config server-config))))))
+      (component/start (new-system config server-config)))))
 
 (defn stop [system]
   (component/stop system))
