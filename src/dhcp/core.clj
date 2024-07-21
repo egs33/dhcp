c45e0d27f8b178d48bcc32f4e7deaf1aeae3f607
egs33 <egs33.bicycle@gmail.com>
2024-07-22 00:49:01 +0900

    Add help option

--- src/dhcp/core.clj
+++ src/dhcp/core.clj
@@ -8,17 +8,26 @@
 (def cli-options
   [["-c" "--config PATH" "path to the configuration file"
     :default "config.yml"]
+   ["-h" "--help" "show this help message"
+    :default false]
    [nil "--debug" nil
     :default false]])
 
 (defn -main
   [& args]
   (let [{:keys [:options :errors :summary]} (cli/parse-opts args cli-options)]
-    (if (seq errors)
+    (cond
+      (:help options)
+      (do (println "\nUsage:")
+          (println summary))
+
+      (seq errors)
       (do (println errors)
           (println "\nUsage:")
           (println summary)
           1)
+
+      :else
       (when-let [{:keys [:udp-server]} (system/start options)]
         ;; block until the server is stopped
         (c.udp-server/blocks-until-close udp-server)))))
