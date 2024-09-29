05d76fbbff1c2a36be7e6203a34d1daee6b9d6ff
egs33 <egs33.bicycle@gmail.com>
2024-09-29 12:11:37 +0900

    Add test

---
+++ test-small/dhcp/http_handler/reservation_test.clj
@@ -0,0 +1,49 @@
+(ns dhcp.http-handler.reservation-test
+  (:require
+   [clojure.test :refer [deftest is testing]]
+   [dhcp.components.database.memory :as db.mem]
+   [dhcp.http-handler :as h]
+   [dhcp.http-handler.reservation]
+   [dhcp.protocol.database :as p.db]
+   [dhcp.test-helper :as th]))
+
+(deftest get-reservations-test
+  (let [db (db.mem/new-memory-database)]
+    (testing "no reservations"
+      (is (= {:status 200, :body {:reservations []}}
+             (h/handler {:db db
+                         :reitit.core/match {:data {:name :get-reservations}}}))))
+    (p.db/add-reservations db [{:hw-address (byte-array [0 11 22 33 44 55])
+                                :ip-address (byte-array [192 168 0 10])
+                                :source "config"}
+                               {:hw-address (byte-array [0 11 22 00 00 00])
+                                :ip-address (byte-array [192 168 0 20])
+                                :source "api"}])
+    (testing "multiple reservations"
+      (is (= {:status 200
+              :body {:reservations [{:hw-address "00:0B:16:21:2C:37"
+                                     :ip-address "192.168.0.10"
+                                     :source "config"}
+                                    {:hw-address "00:0B:16:00:00:00"
+                                     :ip-address "192.168.0.20"
+                                     :source "api"}]}}
+             (h/handler {:db db
+                         :reitit.core/match {:data {:name :get-reservations}}}))))))
+
+(deftest add-reservation-test
+  (let [db (db.mem/new-memory-database)]
+    (testing "reservation added"
+      (is (= {:status 201
+              :body {:hw-address "00:0B:16:21:2C:37"
+                     :ip-address "192.168.0.10"
+                     :source "api"}}
+             (h/handler {:db db
+                         :reitit.core/match {:data {:name :add-reservation}}
+                         :parameters {:body {:hw-address "00:0B:16:21:2C:37"
+                                             :ip-address "192.168.0.10"}}}))
+          "response")
+      (is (= (th/array->vec-recursively [{:hw-address (byte-array [0 11 22 33 44 55])
+                                          :ip-address (byte-array [192 168 0 10])
+                                          :source "api"}])
+             (th/array->vec-recursively (p.db/get-all-reservations db)))
+          "database"))))
