d9f9c50ff7bf29c87e720401a5434a2eb2743218
egs33 <egs33.bicycle@gmail.com>
2024-12-29 00:51:18 +0900

    Add webhook component

--- src/dhcp/components/handler.clj
+++ src/dhcp/components/handler.clj
@@ -2,6 +2,7 @@
   (:require
    [com.stuartsierra.component :as component]
    [dhcp.components.socket]
+   [dhcp.components.webhook]
    [dhcp.handler :as h]
    [dhcp.handler.dhcp-decline]
    [dhcp.handler.dhcp-discover]
@@ -11,6 +12,8 @@
    [dhcp.protocol.database]
    [dhcp.records.config])
   (:import
+   (dhcp.components.webhook
+    IWebhook)
    (dhcp.protocol.database
     IDatabase)
    (dhcp.records.config
@@ -18,15 +21,18 @@
    (dhcp.records.dhcp_packet
     DhcpPacket)))
 
-(defn make-handler [^IDatabase db ^Config config]
+(defn make-handler [^IDatabase db
+                    ^Config config
+                    ^IWebhook webhook]
   (fn [^DhcpPacket message]
     (h/handler {:db db
-                :config config}
+                :config config
+                :webhook webhook}
                message)))
 
-(defrecord Handler [handler config db]
+(defrecord Handler [handler config db webhook]
   component/Lifecycle
   (start [this]
-    (assoc this :handler (make-handler db config)))
+    (assoc this :handler (make-handler db config webhook)))
   (stop [this]
     (assoc this :handler nil)))
