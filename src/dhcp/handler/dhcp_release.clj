6527aad781898f9414f211cf3a427161a6286bd8
egs33 <egs33.bicycle@gmail.com>
2025-07-27 23:18:29 +0900

    Add dhcprelease ciaddr check

--- src/dhcp/handler/dhcp_release.clj
+++ src/dhcp/handler/dhcp_release.clj
@@ -4,6 +4,7 @@
    [dhcp.components.socket]
    [dhcp.const.dhcp-type :refer [DHCPRELEASE]]
    [dhcp.handler :as h]
+   [dhcp.util.bytes :as u.bytes]
    [dhcp.protocol.database :as p.db]
    [dhcp.protocol.webhook :as p.webhook])
   (:import
@@ -17,9 +18,13 @@
    ^DhcpPacket packet]
   (log/debugf "DHCPRELEASE %s" (:message packet))
   (let [message (:message packet)]
-    (when-let [lease (p.db/update-lease db
-                                        (byte-array (:chaddr message))
-                                        (:ciaddr message)
-                                        {:expired-at (Instant/now)})]
-      (p.webhook/send-release webhook lease))
+    ;; Some implementations ciaddr may be zero
+    ;; TODO: use client-id option if available
+    (if (pos? (u.bytes/bytes->number (:ciaddr message)))
+      (when-let [lease (p.db/update-lease db
+                                          (byte-array (:chaddr message))
+                                          (:ciaddr message)
+                                          {:expired-at (Instant/now)})]
+        (p.webhook/send-release webhook lease))
+      (log/debugf "DHCPRELEASE ciaddr is zero, skipping release for %s" (:chaddr message)))
     nil))
